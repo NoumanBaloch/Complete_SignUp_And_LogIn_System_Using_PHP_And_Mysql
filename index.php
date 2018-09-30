98623566827daa94cecdbd10ec9795e8fc8e5c17
Nouman Baloch <mrnoumanbaloch@gmail.com>
2018-09-30 10:08:51 -0700

    A complete signup and login system
    Complete login and signup system using php and mysql databases.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require 'config/config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome</title>
+</head>
+<body>
+	<h1>Hello</h1>
+</body>
+</html>
